8d0fecc8c02e998d54a4e65838b8d38f539bd19e
Jean-François Genillier <jean-francois.genillier@open-groupe.com>
2018-10-16 13:59:50 +0200

    Correct Solution 1 and add Solution 3 to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import com.company.Provided.Fruit;
+import java.util.EnumMap;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
-        solution1.predicateCrimes();
+        solution1.predictCrimes();
 
         Solution2 solution2 = new Solution2();
         solution2.throwDice(2,6);
@@ -12,8 +15,13 @@ public class Main {
         solution2.throwDice(3,6);
         solution2.throwDice(4,10);
 
-//        Solution3 solution3 = new Solution3();
-//        EnumMap<Provided.Fruit, Integer> fruits = new EnumMap<Provided.Fruit, Integer>()
-//        solution3.fruitSalad()
+        Solution3 solution3 = new Solution3();
+        EnumMap<Fruit, Integer> fruits = new EnumMap<Provided.Fruit, Integer>(Fruit.class);
+        fruits.put(Fruit.APPLE, 1);
+        fruits.put(Fruit.APRICOT, 5);
+        fruits.put(Fruit.BANANA, 2);
+        fruits.put(Fruit.ORANGE, 5);
+        fruits.put(Fruit.PEAR, 3);
+        System.out.println(solution3.fruitSalad(fruits));
     }
 }
--- src/com/company/Solution1.java
+++ src/com/company/Solution1.java
@@ -6,33 +6,36 @@ import java.util.Random;
 
 public class Solution1 {
 
-    private static final String EOL = "\n";
+    private static final String EOL = " \n";
 
-    public String predicateCrimes() {
+    public String predictCrimes() {
         StringBuilder result = new StringBuilder();
-        result.append("Crime predicted: ");
-        result.append(generatePhone()) ;
-        result.append(EOL);
-        result.append("Calling: ");
-        result.append(generateSocial());
+        for (int i = 0; i < 30; i++) {
+            result.append("Crime predicted: ");
+            result.append(generateSocial());
+            result.append(" Calling: ");
+            result.append(generatePhone());
+            result.append(EOL);
+        }
         System.out.println(result.toString());
-        result.append(EOL);
         return result.toString();
     }
 
+    // Format : 1-###-555-###
     private String generatePhone() {
         List<String> numbers = new ArrayList<>();
+        numbers.add("1");
         numbers.add(randomStringNumber(3));
         numbers.add("555");
         numbers.add(randomStringNumber(4));
         return String.join("-", numbers);
     }
 
+    // Format : ###-##-####
     private String generateSocial() {
         List<String> numbers = new ArrayList<>();
-        numbers.add("1");
-        numbers.add(randomStringNumber(2));
         numbers.add(randomStringNumber(3));
+        numbers.add(randomStringNumber(2));
         numbers.add(randomStringNumber(4));
         return String.join("-", numbers);
     }
